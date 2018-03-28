a057246c3c4bf9b6f16fb84763c72ebbc3746946
yuanyican <max.yuan.ivan@gmail.com>
2018-03-28 09:18:49 +0800

    init

---
+++ pages/movie/movie.js
@@ -0,0 +1,91 @@
+// pages/movie/movie.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    boards: [
+      {title: '近期上映'},
+      {title: '热门电影'},
+      {title: '电视剧'},
+      {title: '综艺'}
+    ],
+    movieList: [{
+      src: 'https://img3.doubanio.com/view/photo/s_ratio_poster/public/p2509643816.webp',
+      name: '捉妖记'
+    },
+    {
+      src: 'https://img3.doubanio.com/view/photo/s_ratio_poster/public/p2509643816.webp',
+      name: '捉妖记'
+    },
+    {
+      src: 'https://img3.doubanio.com/view/photo/s_ratio_poster/public/p2509643816.webp',
+      name: '捉妖记'
+    },
+    {
+      src: 'https://img3.doubanio.com/view/photo/s_ratio_poster/public/p2509643816.webp',
+      name: '捉妖记'
+    },
+    {
+      src: 'https://img3.doubanio.com/view/photo/s_ratio_poster/public/p2509643816.webp',
+      name: '捉妖记'
+    }]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
