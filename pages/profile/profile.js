4f89248c353b5242b2a637abb2d8a9ef320faeec
yuanyican <max.yuan.ivan@gmail.com>
2018-03-29 14:51:07 +0800

    add profile page

--- pages/profile/profile.js
+++ pages/profile/profile.js
@@ -8,59 +8,21 @@ Page({
   
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
+  loginHandler () {
+    wx.showModal({
+      title: '想登录？',
+      content: '？',
+      success: function (res) {
+        if (res.confirm) {
+          wx.showToast({
+            title: '登录是不可能的'
+          })
+        } else if (res.cancel) {
+          wx.showToast({
+            title: '取消是可以的'
+          })
+        }
+      }
+    })
   }
 })
